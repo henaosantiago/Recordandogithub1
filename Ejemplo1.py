531b86d13aff3878551e564b00ace2721b65989e
Jhony Alexis Martinez <jhonyalexis100@gmail.com>
2023-08-15 08:06:13 -0500

    feat:creamos variables primitivas y lista de datos

---
+++ Ejemplo1.py
@@ -0,0 +1,12 @@
+nombreUsuario = "santiago henao"
+edadUsuario = 23
+estaTuraUsuario = 1.78
+esHinchaDelVerde= True
+
+print (f"Su nombre es : { nombreUsuario} y tiene {edadUsuario} años.")
+
+
+
+comidasFavoritas=["sancocho","hamburguesa","pollo"]
+print(comidasFavoritas)
+print (comidasFavoritas [2] )
